496ffdf969ffd7881e1e302f6c4a1e3147ea26e7
Sebastian V Colom <40442087+sebastian-colom-417@users.noreply.github.com>
2021-01-13 13:58:31 -0800

    JSON parsing and Bokeh plotting

--- src/experiments/views.py
+++ src/experiments/views.py
@@ -103,23 +103,33 @@ from .models import Experiment
 from .serializers import ExperimentSerializer
 
 from django.shortcuts import render
-# from django.http import HttpResponse, JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.parsers import JSONParser
-# from .models import Experiment
-# from .serializers import ExperimentSerializer
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Experiment
+from .serializers import ExperimentSerializer
 
 # View experiment data using bokeh
 
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import HoverTool
+from scipy import stats
+import numpy as np
 
 def experiment_data_view(request, run, *args, **kwargs):
+    experiments = Experiment.objects.filter(run=run)
+    experiment_parameters = {
+        "sensor_p" : experiments[0].sensor,
+        "run_p" : experiments[0].run,
+        "data_type_p" : experiments[0].data_type,
+        "start_time_p" : experiments[0].created,
+        "end_time_p" : experiments[len(experiments) - 1].created
+    }
+
     y = []
     t = []
     v = []
-    experiments = Experiment.objects.filter(run=run)
     t_start_micro = experiments[0].created.microsecond
     t_start_sec = experiments[0].created.second
     t_start_min = experiments[0].created.minute
@@ -128,9 +138,11 @@ def experiment_data_view(request, run, *args, **kwargs):
         t_sec = experiments[i].created.second
         t_min = experiments[i].created.minute
         t_total = (t_micro * 1e-6) + t_sec + (t_min * 60) - ((t_start_micro * 1e-6) + t_start_sec + (t_start_min * 60))
+        if (t_total < 0):
+            t_total = t_total + 3600
         t.append(t_total)
-        v.append(experiments[i].battery_volt)
-        y.append(experiments[i].data)
+        v.append(float(experiments[i].battery_volt))
+        y.append(float(experiments[i].data))
     time_start = experiments[0].created
     title_temp = 'Time-Series of Temperature for Run: ' + str(run) + ', Start Time : ' + str(time_start)
     title_bat = 'Time-Series of Battery Voltage for Run: ' + str(run) + ', Start Time : ' + str(time_start)
@@ -142,14 +154,22 @@ def experiment_data_view(request, run, *args, **kwargs):
     plot_bat.line(t, v, legend="Voltage", line_color="red")
     plot_bat.circle(t, v, fill_color="white", line_color="red", size=8)
 
+    slope, intercept, r_value, p_value, std_err = stats.linregress(t, v)
+    v_lin = (slope*np.array(t) + intercept).tolist()
+    plot_bat.line(x=t, y=v_lin, legend="lin reg slope = {}".format(slope), line_color="green")
+
+    x = np.linspace(0, len(t) - 1, len(t))
+    transmission_rate, irr1, irr2, irr3, irr4 = stats.linregress(x, t)
+    experiment_parameters["trans_rate"] = 1 / transmission_rate
+
     script_t, div_t = components(plot_temp)
     script_b, div_b = components(plot_bat)
+    experiment_parameters["div_t"] = div_t
+    experiment_parameters["div_b"] = div_b
+    experiment_parameters["script_t"] = script_t
+    experiment_parameters["script_b"] = script_b
 
-
-    return render(request, 'data.html', {'script_t': script_t,
-                                         'div_t': div_t,
-                                         'script_b': script_b,
-                                         'div_b': div_b})
+    return render(request, 'data.html', experiment_parameters)
 
 # More concise and uses named status codes
 @api_view(['GET', 'POST'])
@@ -165,10 +185,11 @@ def experiment_list(request, format=None):
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = ExperimentSerializer(data=request.data)
+        data = JSONParser().parse(request)
+        serializer = ExperimentSerializer(data=data)
         if serializer.is_valid():
-            serializer.validated_data['data'] = float(serializer.validated_data.get('data')) / 1.0e6
-            serializer.validated_data['battery_volt'] = float(serializer.validated_data.get('battery_volt')) / 1.0e6
+            # serializer.validated_data['data'] = float(serializer.validated_data.get('data')) / 1.0e6
+            # serializer.validated_data['battery_volt'] = float(serializer.validated_data.get('battery_volt')) / 1.0e6
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -184,20 +205,6 @@ def experiment_list(request, format=None):
 #         return JsonResponse(serializer.data, safe=False)
 #
 #     elif request.method == 'POST':
-#
-#         # identifier_input = request.POST.get('sensor')
-#         # data_type_input = request.POST.get('data_type')
-#         # value_input = request.POST.get('data')
-#         #
-#         # print('method is POST')
-#         #
-#         # if (request.POST.get('sensor') and request.POST.get('data_type') and request.POST.get('data')):
-#         #     print('GOT IT')
-#         #     temp = int(value_input)
-#         #     temp = temp / 1.0e6
-#         #     value_input = str(temp)
-#         #     Experiment.objects.create(sensor = identifier_input, data_type = data_type_input, data = value_input)
-#
 #         data = JSONParser().parse(request)
 #         serializer = ExperimentSerializer(data=data)
 #         if serializer.is_valid():
@@ -212,12 +219,12 @@ def experiment_detail(request, pk, format=None):
     Retrieve, update or delete a experiment.
     """
     try:
-        experiment = Experiment.objects.get(pk=pk)
+        experiment = Experiment.objects.filter(run=pk)
     except Experiment.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = ExperimentSerializer(experiment)
+        serializer = ExperimentSerializer(experiment, many=True)
         return Response(serializer.data)
 
     elif request.method == 'PUT':
